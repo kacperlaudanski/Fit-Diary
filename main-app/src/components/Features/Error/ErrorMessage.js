590fa0486b9b3003d0bab790f4f2a02cae96b3f5
Kacper <kacper.laudanski@wp.pl>
2024-03-20 14:25:26 +0100

    structure improvement

--- main-app/src/components/Features/Error/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./styles/errors.module.css";
-
-const ErrorMessage = (props) => {
-  return <small className={styles.error_message}>{props.children}</small>;
-};
-
-
-export default ErrorMessage; 
--- main-app/src/components/Features/Form/FormWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Form = (props) => {
-    return (
-       <form className={props.className}>{props.children}</form>
-    )
-}
-
-export default Form; 
